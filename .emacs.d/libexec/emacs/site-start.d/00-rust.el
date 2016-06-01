2393f8aece6e7af0cff142e8f28f6945646d8e77
André Ferraz <andre.ferraz@locaweb.com.br>
2016-06-01 19:12:11 -0300

    small bugfix on syntax

--- .emacs.d/libexec/emacs/site-start.d/00-rust.el
+++ .emacs.d/libexec/emacs/site-start.d/00-rust.el
@@ -2,7 +2,7 @@
 (unless (package-installed-p 'rust-mode)
   (package-install 'rust-mode))
 (unless (package-installed-p 'flycheck-rust)
-  (package-install 'flycheck-rust)))
+  (package-install 'flycheck-rust))
 (setq racer-cmd "~/.cargo/bin/racer")
 (setq racer-rust-src-path "~/project/rust/src") ;; Rust source code PATH
 (add-hook 'rust-mode-hook #'racer-mode)
