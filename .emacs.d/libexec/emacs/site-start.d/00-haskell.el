fb6a80d62e997d9f6a45eaadd5f7b11ba5b5c05f
Andre Ferraz <deferraz@terra.com.br>
2016-06-12 19:44:53 -0300

    using intero mode for haskell

--- .emacs.d/libexec/emacs/site-start.d/00-haskell.el
+++ .emacs.d/libexec/emacs/site-start.d/00-haskell.el
@@ -1,3 +1,6 @@
 ; -*- mode: emacs-lisp; -*-
-(unless (package-installed-p 'haskell-mode)
-  (package-install 'haskell-mode))
+;;(unless (package-installed-p 'haskell-mode)
+;;  (package-install 'haskell-mode))
+(unless (package-installed-p 'intero)
+  (package-install 'intero))
+(add-hook 'haskell-mode-hook 'intero-mode)
