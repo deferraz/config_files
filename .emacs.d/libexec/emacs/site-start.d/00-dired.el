8e942ddb5cc9e0d13f8a7ad88cc7455b7c02b8b2
André Ferraz <andre.ferraz@locaweb.com.br>
2017-12-08 12:40:50 -0200

    add dired-sidebar module

--- .emacs.d/libexec/emacs/site-start.d/00-dired.el
+++ .emacs.d/libexec/emacs/site-start.d/00-dired.el
@@ -1,4 +1,8 @@
 ; -*- mode: emacs-lisp; -*-
-(unless (package-installed-p 'dired+)
-  (package-install 'dired+))
-(require 'dired+)
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :config
+  (setq dired-sidebar-use-all-the-icons nil)
+  (setq dired-sidebar-use-term-integration t))
